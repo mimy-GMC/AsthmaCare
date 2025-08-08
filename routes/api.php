db630c37d680d59d9a402e328a70f3643c52b53d
MiryaGAK <celestegakosso234@gmail.com>
2025-08-08 14:05:46 +0200

    Ajout des routes d'authentification d'api

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
