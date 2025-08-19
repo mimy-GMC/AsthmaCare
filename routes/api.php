cb91f2306298fbc12c77d75e625750435d0ce88b
Mimy_GMC <miryammantseke@gmail.com>
2025-08-19 23:29:28 +0200

    Modification des routes d'authentification en dehors du middleware, afin de permettre à l'utilisateur d'appeler les routes register ou login.

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,13 @@ use App\Http\Controllers\AirQualiteController;
 use App\Http\Controllers\ConseilController;
 use App\Http\Controllers\AuthController;
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
 Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+
     // Symptomes
     Route::get('/symptomes', [SymptomeController::class, 'index']);
     Route::post('/symptomes', [SymptomeController::class, 'store']);
@@ -40,11 +46,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/conseils/{id}', [ConseilController::class, 'show']);
     Route::put('/conseils/{id}', [ConseilController::class, 'update']);
     Route::delete('/conseils/{id}', [ConseilController::class, 'destroy']);
-
-
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
