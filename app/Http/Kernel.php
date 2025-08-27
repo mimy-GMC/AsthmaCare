924e6515e14fde2d487ae2efd5ee397fa60a7cbd
Mimy_GMC <miryammantseke@gmail.com>
2025-08-27 15:25:13 +0200

    Configuration dans Kernel.php ajout du middleware comme alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'sync.auth' => \App\Http\Middleware\SyncWebAndAPIAuth::class,
+        'api.format' => \App\Http\Middleware\ApiResponseFormatter::class,
     ];
 }
